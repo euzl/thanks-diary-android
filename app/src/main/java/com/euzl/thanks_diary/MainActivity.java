bba0b2a4e81f8e52bef929b85599d2ddd3ec06ad
euzl <euzl@khu.ac.kr>
2020-04-09 05:41:28 +0900

    [Issue #2] 메인액티비티에 Moment리스트 적용
    - listview를 통해 띄운다.
    - 임시로 함수를 통해 데이터를 넣어줬다. -> DB를 이용하고, 사용자로부터 입력받을 필요가 있음
    - 암튼 잘했다!

--- app/src/main/java/com/euzl/thanks_diary/MainActivity.java
+++ app/src/main/java/com/euzl/thanks_diary/MainActivity.java
@@ -3,12 +3,44 @@ package com.euzl.thanks_diary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<MomentData> momentDataList;
+
+    // TODO: 2020-04-09 momentDataList 개수받아와서 총 n번의 순간으로. 달 구분하는거는 좀 더 ㅠㅠ
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitializeMomentData();
+
+        ListView listView = (ListView)findViewById(R.id.thanksListView);
+        final MomentAdapter mAdapter = new MomentAdapter(this, momentDataList);
+
+        listView.setAdapter(mAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "이거클릭 : " + mAdapter.getItem(position).getThing(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // TODO: 2020-04-09 다이얼로그에 입력하면 momentDataList에 추가되도록 구현
+    public void InitializeMomentData() {
+        momentDataList = new ArrayList<>();
+
+        momentDataList.add(new MomentData(9, "최고로고맙"));
+        momentDataList.add(new MomentData(8, "최고로ㅇ고맙"));
+        momentDataList.add(new MomentData(7, "최고로ㅇㅇㅇ고맙"));
     }
 }
